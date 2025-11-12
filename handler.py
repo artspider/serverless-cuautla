721f243125f10d8f4a9de71bcfa8201768544571
Arturo Rodriguez <arturo.itiguala@gmail.com>
2025-11-11 23:00:07 -0600

    version changed

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+        "message": "Go Serverless v3.0! Your function executed successfully!",
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
